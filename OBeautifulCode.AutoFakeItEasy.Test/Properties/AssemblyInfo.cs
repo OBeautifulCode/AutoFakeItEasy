3fddf4f874f1e30f84c7a6d7a70590fc9c9676e9
Suraj Gupta <surajg@gmail.com>
2015-12-04 14:19:18 -0500

    added test project

---
+++ OBeautifulCode.AutoFakeItEasy.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,43 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="AssemblyInfo.cs" company="OBeautifulCode">
+//   Copyright (c) OBeautifulCode. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("OBeautifulCode.AutoFakeItEasy.Test")]
+[assembly: AssemblyDescription("OBeautifulCode.AutoFakeItEasy.Test")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("OBeautifulCode")]
+[assembly: AssemblyProduct("OBeautifulCode.AutoFakeItEasy.Test")]
+[assembly: AssemblyCopyright("Copyright © 2015 OBeautifulCode")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("f81f7a20-0b67-48c3-91d5-1b4313d06ac2")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: CLSCompliant(true)]
