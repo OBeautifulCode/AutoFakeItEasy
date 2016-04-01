f5fc3cebd205b3408b1ecc64925738710bbb146e
Suraj Gupta <surajg@gmail.com>
2016-04-01 16:15:45 -0400

    add ThatIs, Whose, and ThatIsNot

---
+++ OBeautifulCode.AutoFakeItEasy.Test/EqualityDummies.cs
@@ -0,0 +1,68 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="EqualityDummies.cs" company="OBeautifulCode">
+//   Copyright (c) OBeautifulCode. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace OBeautifulCode.AutoFakeItEasy.Test
+{
+    using System;
+    using System.Diagnostics.CodeAnalysis;
+
+#pragma warning disable SA1649 // File name must match first type name
+#pragma warning disable SA1402 // File may only contain a single class
+    public class AllInstancesEqual : IEquatable<AllInstancesEqual>
+    {
+        public static bool operator ==(AllInstancesEqual left, AllInstancesEqual right)
+        {
+            if (ReferenceEquals(left, right))
+            {
+                return true;
+            }
+
+            if (ReferenceEquals(left, null) || ReferenceEquals(right, null))
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public static bool operator !=(AllInstancesEqual left, AllInstancesEqual right) => !(left == right);
+
+        public bool Equals(AllInstancesEqual other) => this == other;
+
+        public override bool Equals(object obj) => this == (obj as AllInstancesEqual);
+
+        public override int GetHashCode() => 0;
+    }
+
+    public class NoInstancesAreEqual : IEquatable<NoInstancesAreEqual>
+    {
+        public static bool operator ==(NoInstancesAreEqual left, NoInstancesAreEqual right)
+        {
+            if (ReferenceEquals(left, right))
+            {
+                return true;
+            }
+
+            if (ReferenceEquals(left, null) || ReferenceEquals(right, null))
+            {
+                return false;
+            }
+
+            return false;
+        }
+
+        public static bool operator !=(NoInstancesAreEqual left, NoInstancesAreEqual right) => !(left == right);
+
+        public bool Equals(NoInstancesAreEqual other) => this == other;
+
+        public override bool Equals(object obj) => this == (obj as NoInstancesAreEqual);
+
+        public override int GetHashCode() => 0;
+    }
+
+#pragma warning restore SA1402 // File may only contain a single class
+#pragma warning restore SA1649 // File name must match first type name
+    }
---
+++ OBeautifulCode.AutoFakeItEasy/ExtensionMethods.cs
@@ -0,0 +1,99 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ExtensionMethods.cs" company="OBeautifulCode">
+//   Copyright (c) OBeautifulCode. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace OBeautifulCode.AutoFakeItEasy
+{
+    using System;
+
+    using Conditions;
+
+    using FakeItEasy;
+
+    /// <summary>
+    /// Some extension methods.
+    /// </summary>
+    public static class ExtensionMethods
+    {
+        /// <summary>
+        /// Returns a reference dummy if it meets a specified condition or
+        /// creates new dummies of the same type until the condition is met.
+        /// </summary>
+        /// <param name="referenceDummy">The reference dummy.</param>
+        /// <param name="condition">A function to test the reference dummy against a condition.</param>
+        /// <param name="maxAttempts">
+        /// The maximum number of times to attempt to create a dummy that meets the condition, before throwing.
+        /// The reference dummy is itself considered the first attempt.  If this method creates a new dummy
+        /// because the condition fails on the reference dummy, that is considered the second attempt.
+        /// If max attempts is zero or negative then the method tries an infinite number of times to meet the condition (DANGER!!!).
+        /// Default is 100.
+        /// </param>
+        /// <typeparam name="T">The type of dummy.</typeparam>
+        /// <returns>Returns the reference dummy if it meets the specified condition.  Otherwise, returns a new dummy that meets the condition.</returns>
+        public static T ThatIs<T>(this T referenceDummy, Func<T, bool> condition, int maxAttempts = 100)
+        {
+            Condition.Requires(condition, nameof(condition)).IsNotNull();
+            int attempts = 1;
+            while (!condition(referenceDummy))
+            {
+                if (attempts == maxAttempts)
+                {
+                    throw new InvalidOperationException("Unable to create a dummy that satisfies the specified condition.");
+                }
+
+                referenceDummy = A.Dummy<T>();
+                attempts++;
+            }
+
+            return referenceDummy;
+        }
+
+        /// <summary>
+        /// Returns a reference dummy if it meets a specified condition or
+        /// creates new dummies of the same type until the condition is met.
+        /// This method does the exact same thing as <see cref="ThatIs{T}(T, Func{T, bool}, int)"/>.
+        /// </summary>
+        /// <param name="referenceDummy">The reference dummy.</param>
+        /// <param name="condition">A function to test the reference dummy against a condition.</param>
+        /// <param name="maxAttempts">
+        /// The maximum number of times to attempt to create a dummy that meets the condition, before throwing.
+        /// The reference dummy is itself considered the first attempt.  If this method creates a new dummy
+        /// because the condition fails on the reference dummy, that is considered the second attempt.
+        /// If max attempts is zero or negative then the method tries an infinite number of times to meet the condition (DANGER!!!).
+        /// Default is 100.
+        /// </param>
+        /// <typeparam name="T">The type of dummy.</typeparam>
+        /// <returns>Returns the reference dummy if it meets the specified condition.  Otherwise, returns a new dummy that meets the condition.</returns>
+        public static T Whose<T>(this T referenceDummy, Func<T, bool> condition, int maxAttempts = 100)
+            => ThatIs(referenceDummy, condition, maxAttempts);
+
+        /// <summary>
+        /// Returns a reference dummy if it is not equal to a comparison dummy or
+        /// creates new dummies of the same type until a dummy is created that does not equal the comparison dummy.
+        /// Uses the comparison dummy's implementation of <see cref="object.Equals(object)"/>
+        /// </summary>
+        /// <param name="referenceDummy">The reference dummy.</param>
+        /// <param name="comparisonDummy">A dummy to compare against.</param>
+        /// <param name="maxAttempts">
+        /// The maximum number of times to attempt to create a dummy that is not equal to the comparison dummy, before throwing.
+        /// The reference dummy is itself considered the first attempt.  If this method creates a new dummy because
+        /// the comparision dummy is equal to the reference dummy, that is considered the second attempt.
+        /// If max attempts is zero or negative then the method tries an infinite number of times to create a dummy
+        /// that is not equal to the comparison dummy (DANGER!!!).
+        /// Default is 100.
+        /// </param>
+        /// <typeparam name="T">The type of dummy.</typeparam>
+        /// <returns>
+        /// Returns the reference dummy if is not equal to the comparison dummy.
+        /// Otherwise, returns a new dummy that that is not equal to the comparison dummy.
+        /// </returns>
+        public static T ThatIsNot<T>(this T referenceDummy, T comparisonDummy, int maxAttempts = 100)
+        {
+            Func<T, bool> condition = t => !Equals(t, comparisonDummy);
+            var result = ThatIs(referenceDummy, condition, maxAttempts);
+            return result;
+        }
+    }
+}
