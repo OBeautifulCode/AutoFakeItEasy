468e18bf05b82f1a58b1c82c9d065bf1c2ee4269
Suraj K. Gupta <surajg@gmail.com>
2016-11-21 18:26:40 -0500

    documentation fix

--- OBeautifulCode.AutoFakeItEasy/Properties/AssemblyInfo.cs
+++ OBeautifulCode.AutoFakeItEasy/Properties/AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("OBeautifulCode.AutoFakeItEasy")]
-[assembly: AssemblyDescription("Makes FakeItEasy Dummies actually useable by wiring-them-up to real-looking, random test data.  Leverages the power of AutoFixture to create anonymous variables.  Fixes some of AutoFixture's poor default behaviors.  Makes unit tests more expressive with less lines of code.")]
+[assembly: AssemblyDescription("Makes FakeItEasy Dummies useful by wiring-them-up to real-looking, random test data.  Leverages the power of AutoFixture to create anonymous variables.  Fixes some of AutoFixture's poor default behaviors.  Makes unit tests more expressive with less lines of code.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("OBeautifulCode")]
 [assembly: AssemblyProduct("OBeautifulCode.AutoFakeItEasy")]
