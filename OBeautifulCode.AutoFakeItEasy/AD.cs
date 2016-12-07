557f10baa310d5e788c4613dd6729a93938e81de
Suraj K. Gupta <surajg@gmail.com>
2016-12-07 09:11:17 -0500

    use FakeItEasy's built-in dummy creator

--- OBeautifulCode.AutoFakeItEasy/AD.cs
+++ OBeautifulCode.AutoFakeItEasy/AD.cs
@@ -7,10 +7,6 @@
 namespace OBeautifulCode.AutoFakeItEasy
 {
     using System;
-    using System.Linq;
-    using System.Reflection;
-
-    using FakeItEasy;
 
     /// <summary>
     /// Provides methods for generating fake objects.
@@ -19,29 +15,21 @@ namespace OBeautifulCode.AutoFakeItEasy
     public static class AD
     // ReSharper restore InconsistentNaming
     {
-        private static readonly MethodInfo FakeItEasyDummyMethod = typeof(A).GetMethods().Single(_ => _.Name == nameof(A.Dummy));
-
 #pragma warning disable SA1300
         /// <summary>
         /// Gets a dummy object of the specified type.
         /// </summary>
-        /// <param name="type">The type of dummy to return.</param>
+        /// <param name="typeOfDummy">The type of dummy to return.</param>
         /// <returns>A dummy object of the specified type.</returns>
         /// <exception cref="ArgumentException">Dummies of the specified type can not be created.</exception>
         // ReSharper disable InconsistentNaming
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA1704:IdentifiersShouldBeSpelledCorrectly", MessageId = "ummy", Justification = "Attempting to get something as close to A.Dummy<T> as possible.")]
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA1709:IdentifiersShouldBeCasedCorrectly", MessageId = "ummy", Justification = "Attempting to get something as close to A.Dummy<T> as possible.")]
-        public static object ummy(Type type)
+        public static object ummy(Type typeOfDummy)
         // ReSharper restore InconsistentNaming
         #pragma warning restore SA1300
         {
-            if (type == null)
-            {
-                throw new ArgumentNullException(nameof(type));
-            }
-
-            var fakeItEasyGenericDummyMethod = FakeItEasyDummyMethod.MakeGenericMethod(type);
-            object result = fakeItEasyGenericDummyMethod.Invoke(null, null);
+            var result = FakeItEasy.Sdk.Create.Dummy(typeOfDummy);
             return result;
         }
     }
